8cf80205c40ae2758ce4aa0e2ec34362f941bdf1
dawkrish <krishnansh710@gmail.com>
2024-09-25 18:04:49 +0530

    server done

---
+++ cmd/server/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+	networkType := os.Getenv("TYPE")
+	host := os.Getenv("HOST")
+	port := os.Getenv("PORT")
+
+	ln, err := net.Listen(networkType, host+":"+port)
+	if err != nil {
+		panic(err)
+	}
+	defer ln.Close()
+	fmt.Printf("Starting %v Server on %v:%v\n", networkType, host, port)
+
+	conn, err := ln.Accept()
+	fmt.Println("Incoming conn: ", conn.LocalAddr())
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	reader(conn)
+}
+
+func reader(conn net.Conn) {
+	for {
+		reader := bufio.NewReader(conn)
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
+
+		n, err := conn.Write([]byte(line))
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("bytes written :", n)
+	}
+}
