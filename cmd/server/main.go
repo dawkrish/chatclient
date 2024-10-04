4620e1626b20dea1268a980e9b1af91d73f7bc7d
dawkrish <krishnansh710@gmail.com>
2024-10-04 22:45:08 +0530

    handle multiple clients

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,6 +9,10 @@ import (
 	"github.com/joho/godotenv"
 )
 
+var (
+	clients = []net.Conn{}
+)
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -25,29 +29,48 @@ func main() {
 	defer ln.Close()
 	fmt.Printf("Starting %v Server on %v:%v\n", networkType, host, port)
 
-	conn, err := ln.Accept()
-	fmt.Println("Incoming conn: ", conn.LocalAddr())
-	if err != nil {
-		fmt.Println(err)
-		panic(err)
-	}
+	msgs := make(chan string)
 
-	reader(conn)
+	for {
+		conn, err := ln.Accept()
+		fmt.Println("Incoming conn: ", conn.LocalAddr())
+		if err != nil {
+			fmt.Println(err)
+			panic(err)
+		}
+		clients = append(clients, conn)
+		go reader(conn, msgs)
+
+		go func() {
+			for {
+				msgRec := <-msgs
+				if msgRec == "exit" {
+					return
+				}
+				for _, c := range clients {
+					if c == conn {
+						continue
+					}
+					n, err := c.Write([]byte(msgRec))
+					if err != nil {
+						panic(err)
+					}
+					fmt.Println("bytes written to ", c, " ", n)
+				}
+			}
+		}()
+	}
 }
 
-func reader(conn net.Conn) {
+func reader(conn net.Conn, msgs chan string) {
 	for {
 		reader := bufio.NewReader(conn)
 		requestFromClient, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Println("Connection Closed")
+			msgs <- "exit"
 			return
 		}
-
-		n, err := conn.Write([]byte(requestFromClient))
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println("bytes written :", n)
+		msgs <- requestFromClient
 	}
 }
